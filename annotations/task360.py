22c1ea12b95a6ea49ea06ff36489346d403e6f18
quasar098 <70716985+quasar098@users.noreply.github.com>
2025-08-01 23:11:27 -0400

    59 byte sol for task #360

--- annotations/task360.py
+++ annotations/task360.py
@@ -2,3 +2,6 @@
 
 t=lambda v:[*map(list,zip(*v))];p=lambda g:t([[t(g)[:4][y][x]|t(g)[:4:-1][y][x]for x in range(10)]for y in range(4)])
 # first sol (117 bytes)
+
+p=lambda g:[[row[x]|row[8-x]for x in range(4)]for row in g]
+# second sol (59 bytes)
