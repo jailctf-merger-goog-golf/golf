eed612228883e22d6bc6baccea77c89741d19e92
quasar098 <70716985+quasar098@users.noreply.github.com>
2025-08-02 16:44:32 -0400

    cashing

--- view-task.py
+++ view-task.py
@@ -7,6 +7,9 @@ if len(argv) == 1:
     task_num = int(input("task num > "))
 else:
     task_num = int(argv[1])
+if os.path.isfile(f"./working/view/task{task_num:03d}.png"):
+    print(f"./working/view/task{task_num:03d}.png", 'already there')
+    exit(0)
 show_legend()
 examples = load_examples(task_num)
 show_examples(examples['train'] + examples['test'])
