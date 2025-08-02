1d3726e23b1a7ab432cb8cc724e54875ecaa415e
quasar098 <70716985+quasar098@users.noreply.github.com>
2025-08-01 23:58:54 -0400

    147 byte sol for task #120

--- sols/task120.py
+++ sols/task120.py
@@ -1,1 +1,1 @@
-def p(g):w=len(g[0]);r=range;return[[[g[y][x],8][1<=x<w-1and 1<=y<len(g)-1and g[y][x]>0and g[y+1][x+1]==g[y-1][x-1]!=0]for x in r(w)]for y in r(len(g))]
+def p(g):w=len(g[0]);r=range;return[[[g[y][x],8][w-1>x>=1<=y<len(g)-1and g[y][x]>0and g[y+1][x+1]==g[y-1][x-1]!=0]for x in r(w)]for y in r(len(g))]
