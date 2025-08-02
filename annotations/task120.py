3048d5afc11f366fe223efd237048cb84211cca2
quasar098 <70716985+quasar098@users.noreply.github.com>
2025-08-01 22:34:03 -0400

    152 byte sol for task #120

---
+++ annotations/task120.py
@@ -0,0 +1,6 @@
+# basically we create a new grid and for each pixel on the new grid we copy over that pixel
+# from the old grid
+
+# BUT, we change it to the number 8 if it is not touching the wall, it is not air/0, and
+# the topleft pixel to it and the bottomright pixel to it are the same color which are
+# both not zero
